ee39793c1f8fdf7c78ab7a607399cac93e77507a
Daniel Suarez <suarez.daniel101@gmail.com>
2023-02-22 12:21:17 -0500

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -20,7 +20,6 @@ class SongsController < ApplicationController
     if params[:artist_id]
     @artist = Artist.find_by(id: params[:artist_id])
     @song = @artist.songs.find_by(id: params[:id])
-    # # @song = Song.find_by(params[:artist_id])
       if @song.nil?
       # flash[:alert] = "Song not found."
       redirect_to artist_songs_path(@artist), alert: "Song not found."
